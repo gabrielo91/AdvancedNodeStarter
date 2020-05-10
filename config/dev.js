52a8d16ea6b802a82688b1dcd73a3b380fe6b06e
Gabriel Ortega <gabrielo_9103@hotmail.com>
2020-05-09 23:36:35 -0500

    fix auth 0 problema

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "744006437520-i6l0aivlbfsdcp7adrkqgdcjpi2qsq0c.apps.googleusercontent.com",
+  googleClientSecret: "gLsxSv8FWuEmaeyhiG_6QRb1",
+  mongoURI:
+    "mongodb://udemy:udemy@covid-cluster-shard-00-00-4fyag.mongodb.net:27017,covid-cluster-shard-00-01-4fyag.mongodb.net:27017,covid-cluster-shard-00-02-4fyag.mongodb.net:27017/test?ssl=true&replicaSet=Covid-cluster-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123",
 };
